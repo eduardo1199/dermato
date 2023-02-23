b21ea227f9dc53e4adbd4ea63d9d1f0efccab87e
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-02-22 21:59:45 -0300

    no overflow x in main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Stake } from './components/Stake';
 
 export function App() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen overflow-x-hidden">
       <Stake />
 
       <div className="flex justify-center bg-c4">
@@ -10,17 +10,17 @@ export function App() {
           <div className="flex flex-1 min-h-[206px] h-[206px] mt-[-103px] items-center justify-around bg-c1 rounded-md">
             <div className="flex flex-col gap-4 items-center">
               <p className="font-bold text-5xl text-c20">+3.000</p>
-              <span>Clientes satisfeitas</span>
+              <span className="font-bold text-xl text-c19">Clientes satisfeitas</span>
             </div>
             <div className="border-2 border-c5 h-[72px]" />
             <div className="flex flex-col gap-4 items-center">
               <p className="font-bold text-5xl text-c20">+15</p>
-              <span>Anos de experiência</span>
+              <span className="font-bold text-xl text-c19">Anos de experiência</span>
             </div>
             <div className="border-2 border-c5 h-[72px]" />
             <div className="flex flex-col gap-4 items-center">
               <p className="font-bold text-5xl text-c20">+10</p>
-              <span>Filiais</span>
+              <span className="font-bold text-xl text-c19">Filiais</span>
             </div>
           </div>
           <div className="flex justify-center items-center flex-1 flex-col">
